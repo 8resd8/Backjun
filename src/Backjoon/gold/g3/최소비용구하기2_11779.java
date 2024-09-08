4b6939b1adbb66fb3bf8174480cff9ddac285209
RESD\dkanf <dkanfjgwls@naver.com>
2024-09-08 17:05:49 +0900

    01너비우선탐색

--- src/Backjoon/gold/g3/최소비용구하기2_11779.java
+++ src/Backjoon/gold/g3/최소비용구하기2_11779.java
@@ -1,16 +1,13 @@
-package 입출력과사칙연산.src.Backjoon.gold.g3;
+package Backjoon.gold.g3;
 
 // https://www.acmicpc.net/problem/11779
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class 최소비용구하기_11779 {
+public class 최소비용구하기2_11779 {
     static class Edge implements Comparable<Edge> {
         int end, cost;
 
@@ -28,7 +25,7 @@ public class 최소비용구하기_11779 {
     static int N, M;
     static ArrayList<ArrayList<Edge>> adj;
     static ArrayList<Integer> result;
-    static int[] dist;
+    static int[] dist, prev;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -39,10 +36,9 @@ public class 최소비용구하기_11779 {
 
         for (int i = 0; i < N + 1; i++) {
             adj.add(new ArrayList<>());
-
         }
 
-        StringTokenizer st = null;
+        StringTokenizer st;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -57,36 +53,38 @@ public class 최소비용구하기_11779 {
 
         dijkstra(start, end);
 
-        System.out.println(dist[end]);
+        List<Integer> path = new ArrayList<>();
+        for (int i = end; i != -1; i = prev[i]) {
+            path.add(i);
+        }
 
-        System.out.println(Arrays.toString(dist));
+        Collections.reverse(path); // 도착지부터 찾아갔으므로 반대로 바꿈
+        StringBuilder sb = new StringBuilder().append(dist[end]).append("\n").append(path.size()).append("\n");
+        for (Integer i : path) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
     }
 
     private static void dijkstra(int start, int end) {
         dist = new int[N + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
+        Arrays.fill(prev, -1);
         PriorityQueue<Edge> pq = new PriorityQueue<>();
         pq.add(new Edge(start, 0));
         dist[start] = 0;
 
         while (!pq.isEmpty()) {
             Edge cur = pq.poll();
-//            int minCost = cur.cost;
-//            int minNode = cur.end;
-            if (dist[cur.end] < cur.cost) continue;
 
             for (Edge next : adj.get(cur.end)) {
                 if (dist[next.end] > cur.cost + next.cost) {
                     dist[next.end] = cur.cost + next.cost;
                     pq.add(new Edge(next.end, dist[next.end]));
-//                    if (minCost > dist[next.end]) {
-//                        minCost = dist[next.end];
-//                        minNode = next.end;
-//                    }
+                    prev[next.end] = cur.end; // 다음 경로에 가장 마지막에 온사람이 최단거리.
                 }
             }
-
-//            result.add(minNode);
         }
     }
 }
