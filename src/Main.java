a6ecabbfcf8ee4f8a22f822cb94821a204168b06
RESD\dkanf <dkanfjgwls@naver.com>
2024-09-25 23:00:49 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -8,43 +8,19 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] tree = new int[N];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i=0;i<N;i++) {
-            tree[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(tree);
-
-        int min = tree[0];
-        int max = tree[N-1];
-        int mid = (max+min) /2;
-
-        int answer = 0;
-        while (true) {
-            int sum = 0;
-            for (int i=N-1;i>=0;i--) {
-                if (tree[i] <= mid) {
-                    break;
-                }
-                sum += tree[i] - mid;
-            }
-
-            if (sum >= M) {
-                answer = mid;
-            }
-
-            if (mid >= max) {
-                break;
-            }
-            mid = (max+mid) / 2 + 1;
-        }
-
-        System.out.println(answer);
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
     }
 }
