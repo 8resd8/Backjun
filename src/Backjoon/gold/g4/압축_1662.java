31c9263280b3856360b4602711d4ace6148aa9d3
김현우 <dkanfjgwls@naver.com>
2024-10-08 17:47:04 +0900

    압축머지

--- src/Backjoon/gold/g4/압축_1662.java
+++ src/Backjoon/gold/g4/압축_1662.java
@@ -12,8 +12,8 @@ public class 압축_1662 {
         Stack<Character> stack = new Stack<>();
         Stack<Integer> multiple = new Stack<>();
 
-        int answer = 0;
         int length = 0;
+        int answer = 0;
         for (char c : input.toCharArray()) {
             // 곱할 값 저장
             if (c == '(') {
@@ -21,20 +21,41 @@ public class 압축_1662 {
             }
 
             if (c == ')') {
-                while (!stack.isEmpty() && stack.peek() != '(') {
-                    stack.pop();
-                    length++;
-                }
-                stack.pop(); // '(' 제거
-                stack.pop(); // 곱할 값 제거
+                while (!multiple.isEmpty()) {
+                    while (!stack.isEmpty() && stack.peek() != '(') {
+                        stack.pop();
+                        length++;
+                    }
+                    stack.pop(); // '(' 제거
+                    stack.pop(); // 곱할 숫자 제거
+                    length = length * multiple.pop();
 
-                answer = multiple.peek() * length;
-                length *= multiple.pop(); // () 안의 길이 늘리기
+                }
+                answer += length;
+                System.out.println("length = " + length);
+                length = 0;
                 continue;
             }
             stack.push(c);
         }
 
+        System.out.println(stack);
         System.out.println(answer + stack.size()); // 남아있는 스택의 길이 더하기
     }
 }
+/*
+15(22)13(92(1111)42(222))
+answer: 60
+
+15(22)13(92(1111)42(222))123(1)45
+answer: 67
+
+4(3(5(1)))1
+answer: 61
+
+3(5(1))1
+answer: 16
+
+ */
+
+
