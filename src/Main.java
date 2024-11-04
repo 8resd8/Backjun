0e008f43c09cce626dfef1f1eab2b6af6e907a0c
김현우 <dkanfjgwls@naver.com>
2024-11-04 14:45:02 +0900

    add

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+package 입출력과사칙연산.src;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
@@ -13,12 +16,16 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
+        String input = br.readLine();
 
-        String input = "appear";
-        // 모든 접미사 정렬 후 K번째 찾기
-        int K = 5;
-        // subTreeSize = ?
-
+        if (input.length() < 3) {
+            System.out.println("CE");
+        } else if (input.charAt(0) == '"' && input.charAt(input.length() - 1) == '"') {
+            for (int i = 1; i < input.length() - 1; i++) {
+                System.out.print(input.charAt(i));
+            }
+        } else {
+            System.out.println("CE");
+        }
     }
 }
