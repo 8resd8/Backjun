d20a00f04529c7c39ef358d7e14bee96533488a5
김현우 <dkanfjgwls@naver.com>
2024-11-20 17:24:37 +0900

    3184 문제랑 똑같은 문제, bfs 결과 활용

---
+++ src/Backjoon/silver/s1/양_3187.java
@@ -0,0 +1,78 @@
+package Backjoon.silver.s1;
+
+// https://www.acmicpc.net/problem/3187
+// 3184 문제랑 똑같다.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 양_3187 {
+    static int R, C, K, V;
+    static char[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        map = new char[R][C];
+        visited = new boolean[R][C];
+
+        for (int i = 0; i < R; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < C; j++) {
+                map[i][j] = input.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (map[i][j] != '#' && !visited[i][j]) {
+                    bfs(i, j);
+                }
+            }
+        }
+
+        System.out.println(K + " " + V);
+    }
+
+    private static void bfs(int a, int b) {
+        Queue<int[]> q = new ArrayDeque<>();
+        visited[a][b] = true;
+        q.add(new int[]{a, b});
+        int k = 0;
+        int v = 0;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            if (map[x][y] == 'k') k++;
+            else if (map[x][y] == 'v') v++;
+
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || nx >= R || ny < 0 || ny >= C || visited[nx][ny] || map[nx][ny] == '#') continue;
+                q.add(new int[]{nx, ny});
+                visited[nx][ny] = true;
+            }
+        }
+
+        if (k > v) {
+            K += k;
+        } else {
+            V += v;
+        }
+    }
+}
