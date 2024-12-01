b9bee2334ff416e868524bf297efcb6cd5359255
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-01 15:38:12 +0900

    main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
-
-        int b = (int) (a * (1 - 0.22));
-        int c = (int) ((a * 0.8) + (a * 0.2) * (1 - 0.22));
-
-        System.out.println(b + " " + c);
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(input[1] - input[0] + " " + input[1]);
     }
 }
