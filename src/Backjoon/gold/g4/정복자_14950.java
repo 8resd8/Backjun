843b927b27c5378df2b6d12ca2145fc8cb40fc3a
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-07 10:34:13 +0900

    가중치를 더해나가는 조건 + 1

---
+++ src/Backjoon/gold/g4/정복자_14950.java
@@ -0,0 +1,76 @@
+package Backjoon.gold.g4;
+
+// https://www.acmicpc.net/problem/14950
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class 정복자_14950 {
+    static class Edge implements Comparable<Edge> {
+        int start, end, cost;
+
+        public Edge(int start, int end, int cost) {
+            this.start = start;
+            this.end = end;
+            this.cost = cost;
+        }
+
+        public int compareTo(Edge o) {
+            return Integer.compare(cost, o.cost);
+        }
+    }
+
+    static int N, M, t;
+    static int[] parent;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        parent = new int[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            pq.add(new Edge(start, end, cost));
+        }
+
+        int cost = 0;
+        int multiply = 0;
+
+        while (!pq.isEmpty()) {
+            Edge cur = pq.poll();
+            if (union(cur.start, cur.end)) {
+                cost += cur.cost + t * multiply++;
+            }
+        }
+
+        System.out.println(cost);
+    }
+
+    private static boolean union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a == b) return false;
+        parent[b] = a;
+        return true;
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+}
