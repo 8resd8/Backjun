9a08414ecd8bc997a7b3e5bd963bab93f01eec98
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-26 11:33:44 +0900

    뺄셈

---
+++ src/Backjoon/bronze/v5/AI선도대학_32951.java
@@ -0,0 +1,13 @@
+package Backjoon.bronze.v5;
+
+// https://www.acmicpc.net/problem/32951
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AI선도대학_32951 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine()) - 2024);
+    }
+}
