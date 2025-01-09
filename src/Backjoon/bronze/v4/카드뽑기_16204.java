32d6712fe4a969563f6e89705547ca586e502e44
resd <dkanfjgwls@naver.com>
2025-01-09 11:19:15 +0900

    배열에 값 담아두기

---
+++ src/Backjoon/bronze/v4/카드뽑기_16204.java
@@ -0,0 +1,16 @@
+package Backjoon.bronze.v4;
+
+// https://www.acmicpc.net/problem/16204
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 카드뽑기_16204 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}
