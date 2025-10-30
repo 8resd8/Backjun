885a8e8f18104455f897fc92079146db8f63ad41
김현우 <dkanfjgwls@naver.com>
2025-10-30 12:39:11 +0900

    참조값 계산

---
+++ src/정처기/Q4.java
@@ -0,0 +1,20 @@
+package 정처기;
+
+public class Q4 {
+	public static void change(String[] data, String s){
+		data[0] = s;
+		s = "Z";
+	}
+
+	public static void main(String[] args) {
+		String data[] = { "A" };
+		String s = "B";
+		change(data, s);
+		System.out.print(data[0] + s);
+	}
+}
+
+/**
+ * data[0] = s, s는 String 불변.
+ *
+ */
