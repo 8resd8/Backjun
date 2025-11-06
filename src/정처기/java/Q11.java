c40702e24651e8f029e55aa347bcca09ad645cbd
김현우 <dkanfjgwls@naver.com>
2025-11-06 22:50:57 +0900

    객체 생성과 static의 차이 이해

---
+++ src/정처기/java/Q11.java
@@ -0,0 +1,28 @@
+package 정처기.java;
+
+// class Person {
+// 	private String name;
+//
+// 	public Person(String val) {
+// 		name = val;
+// 	}
+// 	public static String get() {
+// 		return name;
+// 	}
+// 	public void print() {
+// 		System.out.println(name);
+// 	}
+// }
+// public class Q11 {
+// 	public static void main(String[] args) {
+// 		Person obj = new Person("Kim");
+// 		obj.print();
+// 	}
+// }
+
+/**
+ * 문제가 되는 라인 번호는?
+ *
+ * static 메소드는 객체 생성없이 사용이 가능한데 name은 초기화 되지 않았음.
+ * 7
+ */
