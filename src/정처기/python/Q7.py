6f348da4d227f47c40a3524f1b4af37386926d0c
김현우 <dkanfjgwls@naver.com>
2025-11-12 21:00:58 +0900

    sum, len 함수

---
+++ src/정처기/python/Q7.py
@@ -0,0 +1,20 @@
+data = [
+    [3, 5, 2, 4, 1],
+    [4, 5, 1],
+    [4, 4, 1, 5, 4],
+    [4, 5]
+]
+
+result = {}
+
+for index, lis in enumerate(data):
+    list_sum = sum(lis)
+    list_len = len(lis)
+
+    result[index] = (list_sum, list_len)
+
+print(result)
+
+
+# 덧셈 15 10 15 9
+# 길이 5 3 5 2
