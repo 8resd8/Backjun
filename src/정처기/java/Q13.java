e8e4f42370d3cae0ecabc5b5857245d4e63df836
김현우 <dkanfjgwls@naver.com>
2025-11-13 17:21:01 +0900

    자동완성에 잊혀질 수 있는 키워드

---
+++ src/정처기/java/Q13.java
@@ -0,0 +1,28 @@
+package 정처기.java;
+/*
+interface Machine {
+	void run();
+}
+
+class WashingMachine (여기에 들어갈 것은?) Machine {
+	private String name;
+
+	public WashingMachine() {
+		this.name = "LG Washer";
+	}
+
+	public void run() {
+		System.out.println("Washing machine running");
+	}
+}
+
+public class Q13 {
+	public static void main(String[] args) {
+		WashingMachine wm = new WashingMachine();
+		wm.run();
+	}
+}
+*/
+
+// implements
+// 자동완성에 당해 까먹을 수 있는 것. extends, implements
